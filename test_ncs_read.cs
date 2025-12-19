0a48858487435acd52ae96af6496387cdf0939bc
Benjamin Auquite <halomastar@gmail.com>
2025-12-18 22:28:01 -0600

    chore: remove obsolete test files and scripts to clean up the repository

--- test_ncs_read.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using Andastra.Parsing.Formats.NCS;
-var reader = new NCSBinaryReader("src/Andastra/Tests/bin/Debug/net9/test-work/roundtrip-work/k1/K1/Data/scripts.bif/01_test.ncs");
-var ncs = reader.Load();
-Console.WriteLine($"Instructions: {ncs.Instructions.Count}");
