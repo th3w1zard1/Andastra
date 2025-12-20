977276dfcb7d813662fda6b773bd7e08525efd1c
Benjamin Auquite <halomastar@gmail.com>
2025-12-19 18:47:33 -0600

    fix: resolve duplicate assembly attributes by using manual AssemblyInfo and excluding Utility obj files

---
+++ src/Andastra/Parsing/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("bolabaden")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("Common shared library for BioWare Aurora engine family games. Provides file format parsers, installation management, and resource access. No dependencies on MonoGame/Stride rendering frameworks.")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta1")]
+[assembly: AssemblyProduct("Andastra.Parsing")]
+[assembly: AssemblyTitle("Andastra.Parsing")]
+[assembly: AssemblyVersion("2.0.0.0")]
