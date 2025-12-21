775cdd845c545ffc61908af43b6a210c37158454
Benjamin Auquite <halomastar@gmail.com>
2025-12-20 22:22:10 -0600

    feat: implement EntityModelRenderer creation in MonoGameEntityModelRenderer
    - Create EntityModelRenderer when GameDataManager and Installation dependencies are provided
    - Add proper null checks and type casting for dependencies
    - Remove TODO stub - renderer is now fully functional when dependencies are available
    - Based on swkotor2.exe: FUN_005261b0 @ 0x005261b0 (model loading from appearance.2da)

--- src/Andastra/Runtime/Graphics/MonoGame/Graphics/MonoGameEntityModelRenderer.cs
+++ src/Andastra/Runtime/Graphics/MonoGame/Graphics/MonoGameEntityModelRenderer.cs
@@ -5,6 +5,9 @@ using Microsoft.Xna.Framework.Graphics;
 using Andastra.Runtime.Core.Interfaces;
 using Andastra.Runtime.Graphics;
 using Andastra.Runtime.MonoGame.Converters;
+using Andastra.Runtime.MonoGame.Rendering;
+using Andastra.Parsing.Installation;
+using GameDataManager = Andastra.Runtime.Engines.Odyssey.Data.GameDataManager;
 using JetBrains.Annotations;
 
 namespace Andastra.Runtime.MonoGame.Graphics
@@ -30,10 +33,20 @@ namespace Andastra.Runtime.MonoGame.Graphics
             _graphicsDevice = device;
             
             // Create the underlying MonoGame renderer if dependencies are provided
-            // TODO: STUB - EntityModelRenderer creation requires Odyssey-specific dependencies
-            // The EntityModelRenderer would be created when proper Odyssey.MonoGame dependencies are available
-            // For now, _renderer remains null and RenderEntity will be a no-op
-            // This matches the original pattern where engine-specific renderers are provided by engine modules
+            // EntityModelRenderer requires both GameDataManager and Installation for model loading
+            // GameDataManager provides access to 2DA tables (appearance.2da, placeables.2da, etc.) for model resolution
+            // Installation provides access to resource system (MDL files, textures, etc.)
+            // Based on swkotor2.exe: FUN_005261b0 @ 0x005261b0 loads creature model from appearance.2da
+            if (gameDataManager != null && installation != null)
+            {
+                GameDataManager gdm = gameDataManager as GameDataManager;
+                Installation inst = installation as Installation;
+                
+                if (gdm != null && inst != null)
+                {
+                    _renderer = new EntityModelRenderer(device, gdm, inst);
+                }
+            }
         }
 
         public void RenderEntity([NotNull] IEntity entity, Matrix4x4 viewMatrix, Matrix4x4 projectionMatrix)
